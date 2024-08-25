49f9ee445d2ff7288135e63234691e60da0fb447
peerapat krairat <peerapat.kra@astrosolutions.co.th>
2024-08-25 15:08:11 +0700

    [update]: bottom components 3d ui

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,13 +9,13 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-primary text-primary-foreground hover:bg-primary/90 border-b-4 border-b-red-700 active:border-b-0",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-background hover:bg-accent hover:text-accent-foreground border-b-4 border-b-neutral-300 active:border-b-0",
         secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+          "bg-secondary text-secondary-foreground hover:bg-secondary/80 border-b-4 border-b-neutral-300 active:border-b-0",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
